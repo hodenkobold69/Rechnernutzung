caad3d17e4e5130f9befdf179c9f3fdbbfee7b65
Unknown <amiculus.umut@hotmail.de>
2017-12-30 00:10:04 +0100

    07 - refined gauss plot, TODOs left

--- gauss.cxx
+++ gauss.cxx
@@ -16,29 +16,37 @@ Double_t Gauss(Double_t *x, Double_t *par)	{
 
 void gauss()	{
 
+	//Create canvas and divide into subpads
 	TCanvas *c1 = new TCanvas("c1", "Gauss Distribution", 500, 500);
 	c1->Divide(2,2);
 
-	//Version 1
+	//Version 1 of Gauss dist.
 	TF1 *ngauss1 = new TF1("ngauss1", Gauss, 0., 10., 2);
 	ngauss1->SetParameter(0, 5.);
 	ngauss1->SetParameter(1, 1.5);
 
-	//Version 2
+	//Version 2 of Gauss dist.
 	TF1 *ngauss2 = new TF1("ngauss2", "1/sqrt(2*pi)/[1]*exp(-0.5*((x-[0])/[1])^2)", 0., 10.);
 	ngauss2->SetParameter(0, 5.);
 	ngauss2->SetParameter(1, 1.5);
 
-	c1->cd(1); ngauss1->Draw();
-	c1->cd(2); ngauss2->Draw();
-	c1->cd(3); ngauss1->DrawDerivative();
-	c1->cd(4); ngauss1->DrawIntegral();
+	//Get pads
+	TPad *c1_1 = (TPad*) c1->GetListOfPrimitives()->FindObject("c1_1");
+	TPad *c1_2 = (TPad*) c1->GetListOfPrimitives()->FindObject("c1_2");
+	TPad *c1_3 = (TPad*) c1->GetListOfPrimitives()->FindObject("c1_3");
+	TPad *c1_4 = (TPad*) c1->GetListOfPrimitives()->FindObject("c1_4");
 
-	TFile *f1 = new TFile("gauss.root", "RECREATE", "dickbutt");
-	c1->Write();
-	ngauss1->Write();
-	ngauss2->Write();
-	f1->Close();
+	/*TODO: SetTitle does not work. TPads actually correspond to their
+	primitive variables (proof: SetLogx() works fine) but the TPaveLabel
+	is not changed properly.*/
+
+	c1_1->cd(); ngauss1->Draw(); c1_1->SetTitle("Gauss (Ver.1)");
+	c1_2->cd(); ngauss2->Draw(); c1_2->SetTitle("Gauss (Ver.2)");
+	c1_3->cd(); ngauss1->DrawDerivative(); c1_3->SetTitle("Derivative");
+	c1_4->cd(); ngauss1->DrawIntegral(); c1_4->SetTitle("Integral");
+
+	//Save everything to a pdf-file
+	c1->Print("gauss.pdf");
 
 }
 
