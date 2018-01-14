ddc5583387215e25dc87a90152127d1dd0122902
Unknown <amiculus.umut@hotmail.de>
2018-01-14 23:20:52 +0100

    08 - list comprehension, man... you gotta love it

--- 08/08 - bootstrap.py
+++ 08/08 - bootstrap.py
@@ -9,7 +9,7 @@ import timeit as time
 N = 10000
 
 s = np.array([np.loadtxt('sample1.dat'), np.loadtxt('sample2.dat')])
-
+print(s)
 def d(s):
 
 	means = [np.mean(x) for x in s]
@@ -20,16 +20,11 @@ def d(s):
 	return np.abs(means[0]-means[1])/sigma
 
 union = np.concatenate(s)
-S1_primes = []
-S2_primes = []
 
 start = time.default_timer()	#time-critical point start
-for i in range(N):
 
-	S1 = [np.random.choice(union) for x in range(len(s[0]))]
-	S2 = [np.random.choice(union) for x in range(len(s[1]))]
-	S1_primes.append(S1)
-	S2_primes.append(S2)
+S1_primes = [[np.random.choice(union) for x in range(len(s[0]))] for i in range(N)]
+S2_primes = [[np.random.choice(union) for x in range(len(s[1]))] for i in range(N)]
 
 dvalues = [d([S1_primes[i], S2_primes[i]]) for i in range(N)]
 d_obs = d(s)
@@ -39,8 +34,10 @@ for i in range(N):
 	if dvalues[i] > d_obs:
 		counter += 1
 
-stop = time.default_timer()	#time-critical point end
+stop = time.default_timer()		#time-critical point end
 
 ratio = (N - counter) / N
+
+#plot some things
 
 print('\nFinished job. Time required: %.2fs\nd_obs = %.2f\nnumber of times, d_obs > d_sampled = %i\nratio (N - counts) / N: %.2f\n' %((stop-start), d_obs, counter, ratio))
